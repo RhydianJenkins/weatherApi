6412e4880fa24f9193456d5c648ba8ec76d30682
Rhydian Jenkins <rhydian.jenkins@lexisnexis.com>
2016-12-08 17:49:29 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+    spl_autoload_register(function ($class) {
+        $baseDir = __DIR__ . '/res/classes/';
+        $file = $baseDir . $class . '.php';
+        if (file_exists($file)) { require $file; }
+    });
+    $weather = new Weather;
+    $availableAreas = $weather->getAvailableAreaNumbers();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="author" content="Rhydian Jenkins">
+<title>Weather API</title>
+<!-- Bootstrap Core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Bootstrap-select -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/css/bootstrap-select.min.css">
+</head>
+<body>
+
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-header">
+                        <h1>Weather API</h1>
+                        <select class="selectpicker show-menu-arrow" data-live-search="true">
+                            <?php foreach ($availableAreas as $name => $number) : ?>
+                                <option><?= $name ?></option>
+                            <?php endforeach ; ?>
+                        </select>
+                    </div>
+                    <h4>Today:</h4>
+                    <p id="item0"></p>
+                    <br />
+                    <h4>Tomorrow:</h4>
+                    <p id="item1"></p>
+                    <br />
+                    <h4>Day After:</h4>
+                    <p id="item2"></p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-header"><h1>Raw API Response</h1></div>
+                    <pre><p id="rawResponse" style="white-space: pre;"><?php var_dump($response); ?></p></pre>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery Version 1.12.4 -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap-select -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/js/bootstrap-select.min.js"></script>
+
+    <!-- Send ajax to custom weather api -->
+    <script>
+        // on page load, populate the thingies
+        // on select, populate the thingies
+        $(document).ready(function(){
+            $('.selectpicker').change(function(e){
+                e.preventDefault();
+                updateWeather();
+            });
+            var updateWeather = function(){
+                $.ajax({
+                    method: 'post',
+                    url: '/ror/weatherApi/res/classes/Weather.php',
+                    data: {
+                        'area': $(".selectpicker").val(),
+                        'ajax': true
+                    },
+                    success: function(response) {
+                        var responseObj = $.parseJSON(response);
+                        var item0 = responseObj.channel.item[0].description;
+                        var item1 = responseObj.channel.item[1].description;
+                        var item2 = responseObj.channel.item[2].description;
+                        var prettyResponse = JSON.stringify(responseObj, null, 4);
+                        $('#item0').html(item0);
+                        $('#item1').html(item1);
+                        $('#item2').html(item2);
+                        $('#rawResponse').html(prettyResponse);
+                    }
+                });
+            }
+            updateWeather();
+        });
+    </script>
+</body>
+</html>
---
+++ res/classes/Weather.php
@@ -0,0 +1,50 @@
+<?php
+
+if ($_POST['area']) {
+    $weather = new Weather;
+    $availableAreaNumbers = $weather->getAvailableAreaNumbers();
+    if (array_key_exists($_POST['area'], $availableAreaNumbers)) {
+        $response = $weather->getWeather($availableAreaNumbers[$_POST['area']]);
+    } else {
+        $response = $weather->getWeather();
+    }
+    echo $response;
+}
+
+class Weather {
+    /**
+     * Gets the weather and returns it json encoded.
+     * Returns FALSE on error.
+     */
+    public function getWeather($areaNumber = 2653822) {
+        $xmlResponse = file_get_contents($this->getUri($areaNumber));
+        try {
+            $xml = simplexml_load_string($xmlResponse);
+            $json = json_encode($xml);
+        } Catch (Exception $e) {
+            // something went wrong!
+            return false;
+        }
+        return $json;
+    }
+
+    /**
+     * Generates a uri depending on the area number given.
+     * Cardiff area number is taken as the default.
+     */
+    public function getUri($areaNumber) {
+        return 'http://open.live.bbc.co.uk/weather/feeds/en/' . $areaNumber . '/3dayforecast.rss';
+    }
+
+    /**
+     * Returns a list of known area numbers
+     */
+    public function getAvailableAreaNumbers() {
+        return array(
+            'Briton Ferry' => '2654668',
+            'Cardiff' => '2653822',
+            'Aberystywth' => '2657782',
+            'Greater London' => '2643743',
+        );
+    }
+}
